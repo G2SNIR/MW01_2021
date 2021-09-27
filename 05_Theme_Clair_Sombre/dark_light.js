32cef650ef0e04483d6a1d2a5b7edfc4179944d7
gaetan <g2.iris.lla@gmail.com>
2021-09-27 17:27:09 +0200

    Modification sur les themes sombres et clairs

--- 05_Theme_Clair_Sombre/dark_light.js
+++ 05_Theme_Clair_Sombre/dark_light.js
@@ -1,16 +1,16 @@
 // Gestion des événements
-document.documentElement.setAttribute('data-theme', 'light');
+//document.documentElement.setAttribute('data-theme', 'light');
 document.getElementById("dark_light").addEventListener("click", darkLight);
 
 function darkLight() {
-    if(document.documentElement.getAttribute('data-theme') == 'light')
+    if(document.getElementById("body").getAttribute('data-theme') == 'light')
     {
-        document.documentElement.setAttribute('data-theme', 'dark');
+        document.getElementById("body").setAttribute('data-theme', 'dark');
         document.getElementById("dark_light").innerHTML="Mode clair";
     }
     else
     {
-        document.documentElement.setAttribute('data-theme', 'light');
+        document.getElementById("body").setAttribute('data-theme', 'light');
         document.getElementById("dark_light").innerHTML="Mode sombre";
     }
 }
